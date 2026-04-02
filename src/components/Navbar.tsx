e2ff4d6c68c5d8f97693f6febb73f31d305c44ef
Krunal <kpdesigneer@gmail.com>
2026-04-03 01:42:12 +0530

    Change Logo to KP

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          KP
         </a>
         <a
           href="mailto:kpfreeworks@mail.com"
